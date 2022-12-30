2c0c70c600b9bf04b214906171c2f4fb58871de5
Sodiq179 <babawalesodiq996@gmail.com>
2022-12-30 15:44:13 +0100

    Update function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -21,10 +21,3 @@ def pascal_triangle(n):
         triangle.append(row)
 
     return triangle
-
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
